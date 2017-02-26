65d0342a0b874c1a6015b1172b63d362164247ee
quangminhtran <minhquang104@gmx.de>
2017-02-26 15:46:51 +0100

    Update bool_expr.ml

--- logic-codes/bool_expr.ml
+++ logic-codes/bool_expr.ml
@@ -4,14 +4,14 @@ type bool_expr =
 	| And of bool_expr * bool_expr
 	| Or of bool_expr * bool_expr;;
 
-(* Evaluation of expression within an environment*)
+(* Evaluation of expression within an environment *)
 let rec eval env = function
 	| Var v -> List.assoc v env
 	| Not e -> not (eval env e)
 	| And (e1,e2) -> (eval env e1) && (eval env e2)
 	| Or (e1,e2) -> (eval env e1) || (eval env e2);;
 
-(* Produce all boolean combinations for variables*)
+(* Produce all boolean combinations for variables *)
 let produce vars = 
 	let rec aux acc = function
 		| [] -> [List.rev acc]
@@ -21,6 +21,6 @@ let produce vars =
 	 		
 (* Truth tables*)
 let table vars expr = 
-	(* For each combination evaluate the expression*)
+	(* For each combination evaluate the expression *)
 	let combinations = produce vars in
 		List.map (function combi -> (combi,  (eval combi expr))) combinations
